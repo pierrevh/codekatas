98aea5cca2c5a892f0f09e4d6e4a88065562b082
Pierre van Hees <pvhees@xs4all.nl>
2019-02-02 18:34:57 +0100

    testthings

---
+++ src/test/java/org/pvhees/teststuff/Base64Things.java
@@ -0,0 +1,10 @@
+package org.pvhees.teststuff;
+
+import java.util.Base64;
+
+public class Base64Things {
+
+    public static void main(String[] args) {
+        System.out.println(new String(Base64.getDecoder().decode("WVFDelhsNHZ2Qmxv")));
+    }
+}
